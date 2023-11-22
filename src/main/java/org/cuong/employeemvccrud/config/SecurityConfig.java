9bd9326816412444d442ed2a0af3cd5d960765c5
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-11-22 21:00:23 +0700

    handling custom login page

--- src/main/java/org/cuong/employeemvccrud/config/SecurityConfig.java
+++ src/main/java/org/cuong/employeemvccrud/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package org.cuong.employeemvccrud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -27,5 +29,23 @@ public class SecurityConfig {
                 .roles("EMPLOYEE", "MANAGER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(ana, topson, ceb);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(configurer -> {
+                configurer.requestMatchers("/assets/**").permitAll();
+                configurer.requestMatchers("/about").permitAll();
+                configurer.requestMatchers("/contact").permitAll();
+                configurer.requestMatchers("/").permitAll();
+                configurer.requestMatchers("").permitAll();
+                configurer.requestMatchers("/employees/**").authenticated();
+            })
+            .formLogin(form -> form
+                    .loginPage("/login")
+                    .loginProcessingUrl("/authenticateTheUser") // no need request mapping required for this
+                    .permitAll());
+        return http.build();
     }
 }
