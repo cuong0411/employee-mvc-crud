8032d41bcddc055bc37777d5165e104e16cd6d5c
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-11-22 14:26:55 +0700

    add bean validation

--- src/main/java/org/cuong/employeemvccrud/controller/EmployeeController.java
+++ src/main/java/org/cuong/employeemvccrud/controller/EmployeeController.java
@@ -1,9 +1,13 @@
 package org.cuong.employeemvccrud.controller;
 
+import jakarta.validation.Valid;
 import org.cuong.employeemvccrud.entity.Employee;
 import org.cuong.employeemvccrud.service.EmployeeService;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -30,9 +34,19 @@ public class EmployeeController {
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute("employee") Employee employee) {
+    public String save(@Valid @ModelAttribute("employee") Employee employee,
+                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println(bindingResult);
+            return "employees/create";
+        }
         employeeService.saveEmployee(employee);
         return "redirect:/employees";
+    }
+
+    @InitBinder
+    public void InitBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
     }
 
     @GetMapping("/update")
--- src/main/java/org/cuong/employeemvccrud/entity/Employee.java
+++ src/main/java/org/cuong/employeemvccrud/entity/Employee.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "employee")
@@ -17,12 +19,16 @@ public class Employee {
     private int id;
 
     @Column(name = "first_name")
+    @NotNull(message = "First name is required")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotNull(message = "Last name is required")
     private String lastName;
 
     @Column(name = "email")
+    @Email(message = "Invalid email")
+    @NotNull(message = "Email is required")
     private String email;
 
     public Employee() {}
