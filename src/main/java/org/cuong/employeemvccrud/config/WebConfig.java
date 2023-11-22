bfa78aea94f00628afbb6b5fe72c741bc5b21c4e
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-11-22 15:00:18 +0700

    add about and contact page

--- src/main/java/org/cuong/employeemvccrud/config/WebConfig.java
+++ src/main/java/org/cuong/employeemvccrud/config/WebConfig.java
@@ -1,12 +1,14 @@
 package org.cuong.employeemvccrud.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("list");
-        registry.addViewController("").setViewName("list");
+        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/contact").setViewName("contact");
     }
 }
