6f82c2921d8166f4f9100c986deeb0ab033a9911
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-11-23 14:01:15 +0700

    add title for login page

--- src/main/java/org/cuong/employeemvccrud/controller/LoginController.java
+++ src/main/java/org/cuong/employeemvccrud/controller/LoginController.java
@@ -1,13 +1,15 @@
 package org.cuong.employeemvccrud.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
+    public String login(Model model) {
+        model.addAttribute("title", "Login Page");
         return "login";
     }
 }
