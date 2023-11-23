298c1bf0b9e482539cf2bf10ccdf62135d3c1360
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2023-11-23 21:52:59 +0700

    add error page for employee not found with invalid id

--- src/main/java/org/cuong/employeemvccrud/controller/HandlerExceptionController.java
+++ src/main/java/org/cuong/employeemvccrud/controller/HandlerExceptionController.java
@@ -4,14 +4,23 @@ package org.cuong.employeemvccrud.controller;
 import org.cuong.employeemvccrud.exception.EmployeeNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @ControllerAdvice
 public class HandlerExceptionController {
 
+//    @ExceptionHandler(EmployeeNotFound.class)
+//    public ResponseEntity<String> findEmployeeById(EmployeeNotFound ex) {
+//        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+//    }
+
     @ExceptionHandler(EmployeeNotFound.class)
-    public ResponseEntity<String> findEmployeeById(EmployeeNotFound ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    @GetMapping("/employee-not-found")
+    public String employeeNotFoundById(EmployeeNotFound ex, Model model) {
+        model.addAttribute("message", ex.getMessage());
+        return "errors/employee-not-found";
     }
 }
